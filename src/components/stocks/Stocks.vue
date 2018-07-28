02c23f0258cac29c3c3b6ef2640831d84f873448
nascal3 <nascal3@gmail.com>
2018-07-29 00:32:34 +0300

    added structure to hold the info about the stock to be traded

--- src/components/stocks/Stocks.vue
+++ src/components/stocks/Stocks.vue
@@ -1,10 +1,26 @@
 <template>
-    <h1>stocks component</h1>
+    <div>
+        <Stock v-for="stock in stocks" :stock="stock"></Stock>
+    </div>
 </template>
 
 <script>
+    import Stock from './Stock.vue';
     export default {
-        name: "stocks"
+        name: "stocks",
+        data() {
+            return {
+                stocks : [
+                    { id: 1, name: 'BMW', price: 110},
+                    { id: 2, name: 'Google', price: 200},
+                    { id: 3, name: 'Apple', price: 250},
+                    { id: 4, name: 'Twitter', price: 8},
+                ]
+            }
+        },
+        components: {
+            Stock
+        }
     }
 </script>
 
