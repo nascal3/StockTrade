9ebbf9fce6fb381bcca7afada401a3da831feb70
nascal3 <nascal3@gmail.com>
2018-07-31 16:54:01 +0300

    added the portfolio store

--- src/store/store.js
+++ src/store/store.js
@@ -2,11 +2,13 @@ import Vue from 'vue';
 import Vuex from 'vuex';
 
 import stocks from './modules/stocks.js';
+import portfolio from './modules/portfolio.js';
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
     modules: {
-        stocks
+        stocks,
+        portfolio
     }
 });
