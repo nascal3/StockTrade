95e22216ea00832aafa142bf7ffcfefa6ddb7207
nascal3 <nascal3@gmail.com>
2018-07-31 15:52:08 +0300

    fetch dummy stock data from store

--- src/components/stocks/Stocks.vue
+++ src/components/stocks/Stocks.vue
@@ -8,19 +8,14 @@
     import Stock from './Stock.vue';
     export default {
         name: "stocks",
-        data() {
-            return {
-                stocks : [
-                    { id: 1, name: 'BMW', price: 110},
-                    { id: 2, name: 'Google', price: 200},
-                    { id: 3, name: 'Apple', price: 250},
-                    { id: 4, name: 'Twitter', price: 8},
-                ]
-            }
-        },
         components: {
             Stock
-        }
+        },
+        computed: {
+            stocks() {
+                return this.$store.getters.stocks;
+            }
+        },
     }
 </script>
 
