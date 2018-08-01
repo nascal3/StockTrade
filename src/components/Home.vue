754a3133e44121022c7607cf0c1a3adfe4d0878e
nascal3 <nascal3@gmail.com>
2018-08-01 17:26:45 +0300

    added text and funds reading to home page

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,10 +1,21 @@
 <template>
-    <h1>home component</h1>
+    <div>
+        <h1>Trade or View your Portfolio</h1>
+        <h6>You may save & Load your Data</h6>
+        <h6>Click on 'End Day' to begin a new day</h6>
+        <hr>
+        <p>Your Funds: {{funds}}</p>
+    </div>
 </template>
 
 <script>
     export default {
-        name: "home"
+        name: "home",
+        computed: {
+            funds() {
+                return this.$store.getters.funds;
+            }
+        },
     }
 </script>
 
