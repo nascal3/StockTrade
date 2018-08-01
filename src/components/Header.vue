1d21dfded92f2bdb0886784a0bf897075d2b48f5
nascal3 <nascal3@gmail.com>
2018-08-02 01:35:44 +0300

    adding action to fetch data from store http connectiion

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -23,7 +23,7 @@
                            aria-expanded="false">Save & Load<span class="caret"></span></a>
                         <ul class="dropdown-menu">
                             <li><a href="#" @click="saveData">Save data</a></li>
-                            <li><a href="#">Load data</a></li>
+                            <li><a href="#" @click="getData">Load data</a></li>
                         </ul>
                     </li>
                 </ul>
@@ -60,6 +60,9 @@
                     stocks: this.$store.getters.stocks
                 };
                 this.$http.put('data.json', data);
+            },
+            getData() {
+                
             }
         }
 
