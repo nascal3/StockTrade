b67c91ddf7687536acdc1155c933dc5a6cb18bd7
nascal3 <nascal3@gmail.com>
2018-08-02 02:01:58 +0300

    added /imported the actions file to the store

--- src/store/store.js
+++ src/store/store.js
@@ -3,10 +3,12 @@ import Vuex from 'vuex';
 
 import stocks from './modules/stocks.js';
 import portfolio from './modules/portfolio.js';
+import * as action from './actions.js';
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
+    action,
     modules: {
         stocks,
         portfolio
