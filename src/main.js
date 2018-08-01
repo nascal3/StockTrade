e5b7cf6c9c359e5e05cccae304d67e4984bade91
nascal3 <nascal3@gmail.com>
2018-08-01 19:40:01 +0300

    added a currency filter

--- src/main.js
+++ src/main.js
@@ -7,6 +7,10 @@ import store from './store/store.js';
 
 Vue.use(VueRouter);
 
+Vue.filter('currency', (value) => {
+    return 'KSh' + value.toLocaleString();
+});
+
 const router = new VueRouter({
     mode: 'history',
     routes
